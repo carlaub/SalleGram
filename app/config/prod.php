16d17792f399c47e7ea9b53de50f09a9fda17c96
jorgejmt94 <jorgemt10@hotmail.com>
2017-04-10 22:03:29 +0200

    2n intent navbar

--- app/config/prod.php
+++ app/config/prod.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\AssetServiceProvider(), array(
     'assets.named_packages' => array(
         'css' => array('base_path' => '/assets/css'),
         'js' => array('base_path' => '/assets/js'),
-        'img' => array('base_urls' => array('http://silex.dev/assets/img')),
+        'img' => array('base_urls' => array('http://grup17.com/assets/img')),
     ),
 
 ));
