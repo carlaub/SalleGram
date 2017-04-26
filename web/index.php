cd258be4ab1024bbd28353e21d225f7291ec4f97
Albertpv95 <albertpv95@icloud.com>
2017-04-26 19:52:59 +0200

    Minor fixes with the image data message.

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 
 ini_set('display_errors', 1);
 
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../app/app.php';
@@ -14,12 +15,17 @@ require __DIR__.'/../app/config/routes.php';
 use pwgram\Model\Entity\User;
 use pwgram\lib\Database\Database;
 use pwgram\Model\Repository\PdoUserRepository;
+use pwgram\Model\Repository\PdoNotificationRepository;
+use pwgram\Model\Entity\Notification;
+use pwgram\Model\AppFormatDate;
+use pwgram\Model\Repository\PdoImageRepository;
+use pwgram\Model\Repository\PdoCommentRepository;
+
 
 date_default_timezone_set('Europe/Madrid');
-/*$db = Database::getInstance("pwgram");
+$db = Database::getInstance("pwgram");
 
-$pdo = new PdoUserRepository($db);*/
-
+$pdo = new PdoUserRepository($db);
 
 $app->run();
 
