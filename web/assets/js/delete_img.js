70d4fb4bd73f440f92cc8b17505e1eeba38002c7
jorgejmt94 <jorgemt10@hotmail.com>
2017-05-08 17:24:43 +0200

    confirm delete image cutre y bug de imagenes del usuario resuelto

--- web/assets/js/delete_img.js
+++ web/assets/js/delete_img.js
@@ -5,14 +5,17 @@ $(document).ready(function() {
     $("#delete-img").click(function (event) {
         var text;
         var r = confirm("Seguro que quiere borrar la imagen?");
-        bootbox.confirm("Are you sure?", function(result){ /* your callback code */ })
 
-        if (r == true) {
-        } else {
-            event.preventDefault();
-        }
+        if (r == true);
+        else event.preventDefault();
 
     });
+
+
+
+
+
+
 
 });
 
